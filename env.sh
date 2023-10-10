72438f66fabb46d3eacaa69cda3f07a293765d40
ggalloro <galloro.giovanni@gmail.com>
2023-10-10 22:48:00 +0200

    Updates

---
+++ env.sh
@@ -0,0 +1,17 @@
+export LOCATION=europe-west1
+export key_gcloud=gcloud
+export KEY=tekton-chains
+export KEYRING=binauthz
+export VERIFIER_SA=image-builder@galloro-demos.iam.gserviceaccount.com
+export CHAINS_NS=tekton-chains
+export KEY_VERSION=1
+export PROJECT=galloro-demos
+export PROJECT_ID=galloro-demos
+export KEY_PROJECT=galloro-demos
+export KMS_URI="gcpkms://projects/${KEY_PROJECT}/locations/${LOCATION}/keyRings/${KEYRING}/cryptoKeys/${KEY}/cryptoKeyVersions/${KEY_VERSION}"
+export IDENTITY=https://kubernetes.io/namespaces/tekton-chains/serviceaccounts/tekton-chains-controller
+export ISSUER=https://container.googleapis.com/v1/projects/galloro-demos/locations/europe-west1/clusters/gke-cluster
+export IMAGE=europe-west1-docker.pkg.dev/galloro-demos/tektoncd/javasample
+export BUILDER=image-builder
+export BUILDER_SA=image-builder@galloro-demos.iam.gserviceaccount.com
+
---
+++ provenance_extractor.sh
@@ -0,0 +1,26 @@
+# Copyright 2023 The Tekton Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Modified to estract taskrun results
+# export IMAGE_URL=$(tkn tr describe --last -o jsonpath="{.status.results[1].value}")
+# export IMAGE_DIGEST=$(tkn tr describe --last -o jsonpath="{.status.results[0].value}")
+# export IMAGE_REF=$IMAGE_URL@$IMAGE_DIGEST
+# gcloud artifacts docker images describe $IMAGE_URL --show-all-metadata --format json | jq -r '.provenance_summary.provenance[0].envelope.payload' | tr '\-_' '+/' | base64 -d | jq > provenance.json
+# gcloud artifacts docker images describe $IMAGE_URL --show-all-metadata --format json | jq -r '.provenance_summary.provenance[0].envelope.signatures[0].sig' | tr '\-_' '+/' | base64 -d | jq > signature
+
+
+# Extract results from pipelinerun
+export IMAGE_URL=$(tkn pr describe --last -o jsonpath="{.status.results[2].value}")
+gcloud artifacts docker images describe $IMAGE_URL --show-all-metadata --format json | jq -r '.provenance_summary.provenance[0].envelope.payload' | tr '\-_' '+/' | base64 -d | jq > provenance
+gcloud artifacts docker images describe $IMAGE_URL --show-all-metadata --format json | jq -r '.provenance_summary.provenance[0].envelope.signatures[0].sig' | tr '\-_' '+/' | base64 -d > signature
