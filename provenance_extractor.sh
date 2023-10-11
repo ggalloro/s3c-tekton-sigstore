054eeeecb15b454b296e95de562d19105e81942a
ggalloro <galloro.giovanni@gmail.com>
2023-10-11 17:28:06 +0200

    MAterial correctly populated here

--- provenance_extractor.sh
+++ provenance_extractor.sh
@@ -21,8 +21,8 @@
 
 
 # Extract results from pipelinerun
-export IMAGE_LOC=$(tkn pr describe --last -o jsonpath="{.status.results[1].value.uri}")
-export IMAGE_SHA=$(tkn pr describe --last -o jsonpath="{.status.results[1].value.digest}")
-export IMAGE_REF=$IMAGE_LOC@$IMAGE_SHA
+export IMAGE_URL=$(tkn pr describe --last -o jsonpath="{.status.results[1].value}")
+export IMAGE_DIGEST=$(tkn pr describe --last -o jsonpath="{.status.results[0].value}")
+export IMAGE_REF=$IMAGE_URL@$IMAGE_DIGEST
 gcloud artifacts docker images describe $IMAGE_REF --show-all-metadata --format json | jq -r '.provenance_summary.provenance[0].envelope.payload' | tr '\-_' '+/' | base64 -d | jq > provenance
 gcloud artifacts docker images describe $IMAGE_REF --show-all-metadata --format json | jq -r '.provenance_summary.provenance[0].envelope.signatures[0].sig' | tr '\-_' '+/' | base64 -d | jq  > signature
